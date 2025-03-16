54f43eafb69e797e231676735875704de05bd064
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-03-16 11:46:32 -0500

    Refactor: Simplify Card component rendering in Main
    Removes unnecessary index prop and simplifies the album
    mapping in the Main component for cleaner rendering.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,15 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 import { Album } from "@/app/Album";
 
 export default function Main({ albums = [] }: { albums: Album[] }) {
-  let i = 0;
   return (
     <main>
-      <ul className='cards'>
-        {
-          albums.map((album: Album) => {
-            return (
-              <Card key={album.id} index={i++}>{album}</Card>
-            )
-          })
-        }
+      <ul className="cards">
+        {albums.map((album: Album) => {
+          return <Card key={album.id}>{album}</Card>;
+        })}
       </ul>
     </main>
-  )
+  );
 }
