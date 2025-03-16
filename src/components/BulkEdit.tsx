1c2022d4dab187efa3c81ba5fabdf8b9f5c67204
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-03-16 11:33:45 -0500

    Add basic bulk edit component
    This commit introduces a simple textarea element
    within a functional component called BulkEdit. This
    component is intended to provide a space for users
    to input and manipulate large amounts of text data.

---
+++ src/components/BulkEdit.tsx
@@ -0,0 +1,7 @@
+export default function BulkEdit() {
+  return (
+    <div className='bulk-edit w-full h-full'>
+      <textarea className='w-full h-full' placeholder='Bulk Edit' />
+    </div>
+  )
+}
