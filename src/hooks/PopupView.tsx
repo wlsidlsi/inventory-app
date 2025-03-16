f96c8bb6a734076beea7af9c9232c1d38b885cd4
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-03-16 11:31:51 -0500

    feat: Implement usePopup hook for modal functionality
    This commit introduces the `usePopup` hook, which provides
    modal/popup functionality.
    
    The hook manages the state of the popup content and provides
    functions to show and close the popup. It also uses
    `ReactDOM.createPortal` to render the popup content into a
    dedicated modal root element in the DOM.
    
    A cleanup effect is included to ensure the popup content is
    reset on unmount.

---
+++ src/hooks/PopupView.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState, useEffect, ReactNode } from "react";
+import ReactDOM from "react-dom";
+
+// Custom Hook: usePopup
+export interface PopupContent {
+  leftBlock: ReactNode;
+  content: ReactNode;
+}
+
+export function usePopup() {
+  const [popupContent, setPopupContent] = useState<PopupContent | null>(null);
+  const showPopup = (content: PopupContent) => setPopupContent(content);
+  const closePopup = () => setPopupContent(null);
+
+  // Cleanup effect when unmounting
+  useEffect(() => {
+    return () => setPopupContent(null);
+  }, []);
+
+  const popupElement = popupContent ? (
+    ReactDOM.createPortal(
+      <div className="fixed inset-0 items-center justify-center bg-blue-950 bg-opacity-95 p-10" onClick={e => {e.stopPropagation(); closePopup()}}>
+        <div
+          className="flex justify-between px-5 mb-5"
+        >
+          <div className="w-[142px]">
+            {popupContent.leftBlock}
+          </div>
+          <div className="w-[42px]">
+            <button className="modal-close">
+            ✖
+            </button>
+          </div>
+        </div>
+        <div className="h-[90%] mx-5 bg-white p-4 rounded" onClick={(e) => e.stopPropagation()}>
+          {popupContent.content}
+        </div>
+      </div>,
+      document.querySelector("#modal-root") as Element
+    )
+  ) : (
+    null
+  );
+
+  return { showPopup, closePopup, popupElement };
+}
