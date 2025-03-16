c64124fdb735be89b38063f84323d70a44cef6f8
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-03-16 11:36:31 -0500

    Refactor: Update addItem and removeAllItems functions
    The addItem function now uses the Album class constructor.
    The removeAllItems function is now called directly.
    Fixes minor UI spacing issue.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,6 +4,7 @@ import { usePopupContext } from "./PopupProvider";
 import { useRef } from "react";
 import BulkEdit from "./BulkEdit";
 import UploadInventory from "./UploadInventory";
+import { Album } from "@/app/Album";
 interface NavigationProps {
   lastAlbumId?: number;
 }
@@ -20,15 +21,15 @@ export default function Navigation({ lastAlbumId }: NavigationProps) {
         <div className="flex flex-grow flex-wrap space-x-4 justify-center lg:justify-start lg:pl-[118px]">
           {["Total Inventory", "Viewing", "Total Pages", "Current Page", "Search Results"].map((item, index) => (
             <div key={index}>
-              <span><strong>{item}:</strong><span className='ml-'>0</span></span>
+              <span><strong>{item}:</strong><span className='pl-4'>0</span></span>
             </div>
           ))}
         </div>
         <div className='flex flex-grow md:w-[340px] space-x-4 justify-center lg:justify-end'>
-          <button id="add" onClick={() => addItem({ artistName: `My album ${Math.random()}`, year: "2023" })}><strong>+</strong></button>
+          <button id="add" onClick={() => addItem(new Album({ artistName: `My album ${Math.random()}` }))}><strong>+</strong></button>
           <button id="remove" className={lastAlbumId == 0 ? "invisible" : "visible"} onClick={() => lastAlbumId && removeItem(lastAlbumId)}><strong>-</strong></button>    
           <button id="bulkEdit" className='min-w-[120px] px-4 py-1' onClick={() => showPopup({ content: <BulkEdit />, leftBlock: uploadForm.current })} >Bulk Edit</button>
-          <button id="clear" className='px-4 py-1' onClick={removeAllItems} onTouchStart={removeAllItems}>Clear</button>
+          <button id="clear" className='px-4 py-1' onClick={() => removeAllItems()}>Clear</button>
         </div>
       </div>
       <hr />
